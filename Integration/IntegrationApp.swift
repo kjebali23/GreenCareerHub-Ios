ec8fe2b0a779183438bc36a7b58fc5f2fa3c00f7
kjebali23 <khalil.jebali.kj@gmail.com>
2023-12-06 14:54:56 +0100

    Initial Commit

---
+++ Integration/IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationApp.swift
+//  Integration
+//
+//  Created by khalil jebali on 6/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
